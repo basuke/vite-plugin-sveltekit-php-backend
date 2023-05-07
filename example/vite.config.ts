a92ba9310610f19a93663381045cc2f6db103bfd
Basuke Suzuki <basuke@mac.com>
2023-05-07 13:08:35 -0700

    Change indent from 2 to 4.

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,7 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import phpBackend from "../src";
-import { defineConfig } from "vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import phpBackend from '../src';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), phpBackend({ address: "localhost:9000" })],
+    plugins: [sveltekit(), phpBackend({ address: 'localhost:9000' })],
 });
--- src/index.ts
+++ src/index.ts
@@ -1,111 +1,122 @@
-import type { Plugin } from "vite";
+import type { Plugin } from 'vite';
 
-import path from "node:path";
-import fs from "node:fs";
-import { createClient } from "fastcgi-kit";
+import path from 'node:path';
+import fs from 'node:fs';
+import { createClient } from 'fastcgi-kit';
 
-const name = "vite-plugin-sveltekit-php-backend";
+const name = 'vite-plugin-sveltekit-php-backend';
 const sharedClientVirtualModule = `virtual:${name}/shared-client`;
-const sharedClientResolvedModuleId = "\0" + sharedClientVirtualModule;
+const sharedClientResolvedModuleId = '\0' + sharedClientVirtualModule;
 
 interface PHPBackendOptions {
-  address?: string;
-  debug?: boolean;
+    address?: string;
+    debug?: boolean;
 }
 
 function posixify(str: string): string {
-  return str.replace(/\\/g, "/");
+    return str.replace(/\\/g, '/');
 }
 
 function flattenId(id: string): string {
-  return posixify(id).replaceAll("/", "_");
+    return posixify(id).replaceAll('/', '_');
 }
 
 function Q(s: string): string {
-  return JSON.stringify(s);
+    return JSON.stringify(s);
 }
 
 /**
  * Returns the PHP Backend for SvelteKit Vite plugin.
  */
 export async function plugin(
-  options: PHPBackendOptions = {}
+    options: PHPBackendOptions = {}
 ): Promise<Plugin[]> {
-  const address = options.address ?? "localhost:9000";
-  const debug = options.debug ?? false;
-  let root;
-  let building = false;
-  let phpDir;
+    const address = options.address ?? 'localhost:9000';
+    const debug = options.debug ?? false;
+    let root;
+    let building = false;
+    let phpDir;
 
-  const plugin: Plugin = {
-    name,
+    const plugin: Plugin = {
+        name,
 
-    configResolved(config) {
-      root = config.root;
-      building = config.command === "build";
-      phpDir = path.join(
-        building ? config.build.outDir : config.cacheDir,
-        "php"
-      );
-      fs.mkdirSync(phpDir, { recursive: true });
-    },
+        configResolved(config) {
+            root = config.root;
+            building = config.command === 'build';
+            phpDir = path.join(
+                building ? config.build.outDir : config.cacheDir,
+                'php'
+            );
+            fs.mkdirSync(phpDir, { recursive: true });
+        },
 
-    buildStart() {},
+        buildStart() {},
 
-    resolveId(id) {
-      if (id === sharedClientVirtualModule) {
-        return sharedClientResolvedModuleId;
-      }
+        resolveId(id) {
+            if (id === sharedClientVirtualModule) {
+                return sharedClientResolvedModuleId;
+            }
 
-      // Error check for client side logic.
-      if (id.endsWith(".php")) {
-        if (id !== "+server.php" && !id.endsWith(".server.php")) {
-          throw new Error("PHP cannot run on client side.");
-        }
-      }
-    },
+            // Error check for client side logic.
+            if (id.endsWith('.php')) {
+                if (id !== '+server.php' && !id.endsWith('.server.php')) {
+                    throw new Error('PHP cannot run on client side.');
+                }
+            }
+        },
 
-    load(id) {
-      if (id === sharedClientResolvedModuleId) {
-        return sharedClientJS({ address, debug, root });
-      }
-    },
+        load(id) {
+            if (id === sharedClientResolvedModuleId) {
+                return sharedClientJS({ address, debug, root });
+            }
+        },
 
-    async transform(code, id) {
-      if (id.endsWith(".php")) {
-        id = path.relative(root, id);
+        async transform(code, id) {
+            if (id.endsWith('.php')) {
+                id = path.relative(root, id);
 
-        const phpPath = path.join(phpDir, flattenId(id));
-        fs.writeFileSync(phpPath, phpBackendMain + code);
+                const phpPath = path.join(phpDir, flattenId(id));
+                fs.writeFileSync(phpPath, phpBackendMain + code);
 
-        const relativePath = path.relative(root, phpPath);
+                const relativePath = path.relative(root, phpPath);
 
-        const client = createClient({
-          address,
-          debug,
-          params: {
-            DOCUMENT_ROOT: root,
-          },
-        });
+                const client = createClient({
+                    address,
+                    debug,
+                    params: {
+                        DOCUMENT_ROOT: root,
+                    },
+                });
 
-        const backendUrl = "http://localhost/" + relativePath;
-        const response = await client.options(backendUrl, {});
-        const config = response.json();
+                const backendUrl = 'http://localhost/' + relativePath;
+                const response = await client.options(backendUrl, {});
+                const config = response.json();
 
-        if (id.endsWith("+server.php")) {
-          const methods = ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"];
-          code = invokePhpEndpointJS(relativePath, methods);
-        } else {
-          code =
-            invokePhpLoadJS(relativePath) +
-            definePhpActionsJS(relativePath, ["update", "enter", "restart"]);
-        }
-        return { code };
-      }
-    },
-    buildEnd() {},
-  };
-  return [plugin];
+                if (id.endsWith('+server.php')) {
+                    const methods = [
+                        'GET',
+                        'POST',
+                        'PUT',
+                        'DELETE',
+                        'PATCH',
+                        'OPTIONS',
+                    ];
+                    code = invokePhpEndpointJS(relativePath, methods);
+                } else {
+                    code =
+                        invokePhpLoadJS(relativePath) +
+                        definePhpActionsJS(relativePath, [
+                            'update',
+                            'enter',
+                            'restart',
+                        ]);
+                }
+                return { code };
+            }
+        },
+        buildEnd() {},
+    };
+    return [plugin];
 }
 
 interface PHPStructure {}
@@ -228,28 +239,27 @@ const invokePhpLoadJS = (phpPath: string) => `
 `;
 
 const invokePhpEndpointJS = (phpPath: string, methods: string[]) =>
-  `
+    `
   import { invokePhpEndpoint } from "${sharedClientVirtualModule}";
 ` +
-  methods
-    .map(
-      (method) => `
+    methods
+        .map(
+            (method) => `
   export async function ${method}(event) {
     return await invokePhpEndpoint(${Q(phpPath)}, ${Q(method)}, event);
   }
 `
-    )
-    .join("\n");
+        )
+        .join('\n');
 //
 const definePhpActionsJS = (phpPath: string, actions: string[]) =>
-  `
-  import { invokePhpActions } from "${sharedClientVirtualModule}";
+    `import { invokePhpActions } from "${sharedClientVirtualModule}";
 
   export const actions = {` +
-  // prettier-ignore
-  actions.map((action) => `
+    // prettier-ignore
+    actions.map((action) => `
     ${Q(action)}: async (event) => invokePhpActions(${Q(phpPath)}, ${Q(action)}, event)`).join(",") +
-  `
+    `
   };
 `;
 
